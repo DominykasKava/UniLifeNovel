279b4d255143a6b71fd812acdb58fb528fecdf79
Benediktas555 <benediktastumas@gmail.com>
2026-03-05 14:48:04 +0200

    Sukurta dialogų prefab

---
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueUI : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+    public Image portrait;
+}
