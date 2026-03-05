d648fa075762a65d478447077a81089b5c924937
labmykolas <lab.mykolas@gmail.com>
2026-03-05 14:34:42 +0200

    sukurta CharacterDisplay clase

---
+++ Assets/Scripts/character/CharacterDisplay.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using TMPro;
+
+public class CharacterDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nameText;
+
+    private void OnEnable()
+    {
+        SpeakerEvents.OnSpeakerChanged += SetSpeakerName;
+    }
+
+    private void OnDisable()
+    {
+        SpeakerEvents.OnSpeakerChanged -= SetSpeakerName;
+    }
+
+    public void SetSpeakerName(string speakerName)
+    {
+        nameText.text = speakerName;
+    }
+}
