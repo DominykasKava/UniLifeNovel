00a395d6fec0dea439c2b53a9c82e3c702d745f3
DominykasKava <domkav@ktu.lt>
2026-03-05 13:39:50 +0200

    Sukūriau DialogueNode klasę

---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class DialogueNode
+{
+    public string id;
+    public string speaker;
+    public string text;
+    public string next;
+    public string portrait;
+    public string background;
+}
