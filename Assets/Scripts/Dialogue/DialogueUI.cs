cf692c3055e19547d805996a33f7ff720e712d4c
Benediktas555 <benediktastumas@gmail.com>
2026-03-10 12:38:48 +0200

    Sukuriau typewrite efekta

--- Assets/Scripts/Dialogue/DialogueUI.cs
+++ Assets/Scripts/Dialogue/DialogueUI.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class DialogueUI : MonoBehaviour
 {
@@ -8,10 +9,31 @@ public class DialogueUI : MonoBehaviour
     public TMP_Text dialogueText;
     public Image portrait;
 
+    public float typingSpeed = 0.03f;
+
+    Coroutine typingCoroutine;
+
     public void DisplayDialogue(string speaker, string line, Sprite portraitSprite)
     {
         nameText.text = speaker;
-        dialogueText.text = line;
         portrait.sprite = portraitSprite;
+
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+
+        typingCoroutine = StartCoroutine(TypeText(line));
+    }
+
+    IEnumerator TypeText(string line)
+    {
+        dialogueText.text = "";
+
+        foreach (char letter in line)
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
     }
 }
