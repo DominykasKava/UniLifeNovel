8a70eacf278100e0d2e4b140568b7e72fdf09cae
labmykolas <lab.mykolas@gmail.com>
2026-05-08 15:17:50 +0300

    padariau gamemanager scripta

---
+++ Assets/Scripts/SceneController/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public int currentChapter = 1;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ResetGame()
+    {
+        currentChapter = 1;
+
+        // čia gali resetinti:
+        // dialogo manager
+        // objectives
+        // variables
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneLoader.Instance.LoadMainMenu();
+    }
+}
