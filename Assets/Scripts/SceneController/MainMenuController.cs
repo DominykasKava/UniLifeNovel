c302a1880740ba09f5626795cb654a0c02e069c6
labmykolas <lab.mykolas@gmail.com>
2026-05-12 23:51:39 +0300

    padariau main meniu kontrolers scripta

---
+++ Assets/Scripts/SceneController/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // čia gali resetinti state jei turi singletonus
+        GameManager.Instance.ResetGame();
+
+        SceneLoader.Instance.LoadGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
